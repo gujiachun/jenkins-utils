f27f8787d21581b2e61e5d4eacca0fb265bb3736
gujiachun <767724839@qq.com>
2017-12-29 16:46:26 +0800

    1111

---
+++ src/main/java/com/rainbow/jenkins/utils/string/TestStr.java
@@ -0,0 +1,15 @@
+package com.rainbow.jenkins.utils.string;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestStr {
+
+	@Value("name")
+	private String name;
+	
+	public String getStr(){
+		return name;
+	}
+}
